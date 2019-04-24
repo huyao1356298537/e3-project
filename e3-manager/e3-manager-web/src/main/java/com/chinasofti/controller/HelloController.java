976506da08e1b14305019baa182f2cf2e76d2a90
huyao1356298537 <1356298537@qq.com>
2019-04-24 15:17:55 +0800

    4.前台页面

--- e3-manager/e3-manager-web/src/main/java/com/chinasofti/controller/HelloController.java
+++ e3-manager/e3-manager-web/src/main/java/com/chinasofti/controller/HelloController.java
@@ -4,8 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author : 胡瑶
- * date : 2019/4/23
+ * author:胡瑶
+ * date:2019/4/24
+ * time:14:53
  */
 @RestController
 public class HelloController {
---
+++ e3-manager/e3-manager-web/src/main/java/com/chinasofti/controller/PageController.java
@@ -0,0 +1,24 @@
+package com.chinasofti.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * author:胡瑶
+ * date:2019/4/24
+ * time:14:53
+ */
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String showIndex() {
+        return "index";
+    }
+
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page) {
+        return page;
+    }
+}
